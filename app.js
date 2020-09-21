bb37e2a9bcd842d62b0bc79909e20b5e5c456bbf
nadinedelia <nadine.delia@yahoo.com>
2020-09-21 16:39:31 +0200

    array types

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'Ejila',
-    age: 28
+    age: 28,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['Yoga'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 const person = {
     name: 'Ejila',
-    age: 28
+    age: 28,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Yoga'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies ) {
+    console.log(hobby.toUpperCase())
+}
